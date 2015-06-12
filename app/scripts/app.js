a03c50eb248358f070319f48d257ca6376dadf55
Arti Mathanda <artismarti@gmail.com>
2015-06-12 14:39:03 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+/* global app:true */
+/* exported app */
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
